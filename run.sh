b3fab91c68f497fd4d97cc09f41554f045e2ad4b
Michelle Oliveira <miolivc@outlook.com>
2017-11-08 11:38:33 -0300

    uso de networks e volumes nos containers docker

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 mvn clean package
 docker-compose up -d
+echo "\nAplicação disponível em http://localhost:8082/app\n"
 # docker build -t ricardojob/banco ./postgres
 # docker run -p 5433:5432 -d --name banco -v $(pwd)/data:/var/lib/postgresql/data ricardojob/banco
 # mvn clean package
