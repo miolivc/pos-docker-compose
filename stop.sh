6b807b6fa02e9d6ea9d6e4f4b7280cd9c5d8e1e8
Michelle Oliveira <miolivc@outlook.com>
2017-11-08 20:31:03 -0300

    Trocando versão do docker-compose e colocando comando docker-compose no stop.sh

--- stop.sh
+++ stop.sh
@@ -1,11 +1,12 @@
-docker stop app
-docker kill app
-docker rm app
-docker rmi -f ricardojob/pos-aula
+#docker stop app
+#docker kill app
+#docker rm app
+#docker rmi -f ricardojob/pos-aula
 
-docker stop banco
-docker kill banco
-docker rm banco
-docker rmi -f ricardojob/banco
+#docker stop banco
+#docker kill banco
+#docker rm banco
+#docker rmi -f ricardojob/banco
 
+docker-compose down
 mvn clean
